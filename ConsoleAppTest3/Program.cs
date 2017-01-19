f1237daa32eaf433a9c0ca2cf7f0e0857b315461
JUHEL <freddy.juhel@sogeti.com>
2017-01-19 17:36:12 +0100

    Initial commit

---
+++ ConsoleAppTest3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleAppTest3
+{
+  internal static class Program
+  {
+    private static void Main()
+    {
+      //code de freddy
+      Console.WriteLine("hello this is freddy's code");
+      // keita can you add code by pulling request it ?
+
+      Console.ReadKey();
+    }
+  }
+}
